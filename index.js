3c405ef172d2af98cbb53c6653ba76b6cabc8efc
desuchann <75581903+desuchann@users.noreply.github.com>
2021-10-09 16:34:10 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+function ingredientsHover() {
+    document.getElementById('ingredients').firstElementChild.firstElementChild.style.fontSize = "300%"
+}
+
+function ingredientsNormal() {
+    document.getElementById('ingredients').firstElementChild.firstElementChild.style.fontSize = "100%"
+}
+
+function prepHover() {
+    document.getElementById('preparation').firstElementChild.firstElementChild.style.fontSize = "300%"
+}
+
+function prepNormal() {
+    document.getElementById('preparation').firstElementChild.firstElementChild.style.fontSize = "100%"
+}
